d3484bdf21e9f2d789ee14ce362f9f33e73806c6
RobertGronlund <robert.gronlund.nielsen@gmail.com>
2024-12-03 09:45:31 +0100

    day2 pt 2

--- day-2/day-2.cpp
+++ day-2/day-2.cpp
@@ -7,29 +7,65 @@
 
 using namespace std;
 
-int find_modifier(const vector<int>& report) {
-    int modifier {};
-    for (size_t i = 0; i < report.size()-1; i++) {
-        if (report[i] - report[i+1] > 0) {
-            modifier--;
-        } else if (report[i] - report[i+1] < 0) {
-            modifier++;
-        }
-    }
-
-    return modifier;
-}
-
 // Utility function to check if a value is within a specified range
 bool is_within_range(int value, int lower, int upper) {
     return value >= lower && value <= upper;
 }
 
+bool safe_check (vector<int>& report, bool increasing) {
+    bool isSafe = true;
+    for (size_t i = 0; i < report.size() - 1; ++i) {
+        if ((report[i] - report[i + 1]) == 0) {
+            isSafe = false;
+            break;
+        }
+        if (increasing) {
+            if (!is_within_range(report[i] - report[i + 1], 1, 3)) {
+                isSafe = false;
+                break;
+            }
+        } else {
+            if (!is_within_range(report[i] - report[i + 1], -3, -1)) {
+                isSafe = false;
+                break;
+            }
+        }
+    }
+    return isSafe;
+}
 
+bool brute_check_report(vector<int>& report) {
 
+    for (size_t i = 0; i < report.size() - 1; i++) {
+
+        bool isSafe = false;
+        bool increasing = true;
+
+        isSafe = isSafe || safe_check(report, increasing);
+        isSafe = isSafe || safe_check(report, !increasing);
+
+        if (isSafe) {
+            return true;
+        } else {
+            for (size_t i = 0; i < report.size(); i++) {
+                auto reportCopy = report;
+                reportCopy.erase(reportCopy.begin() + i);
+
+                isSafe = isSafe || safe_check(reportCopy, increasing);
+                isSafe = isSafe || safe_check(reportCopy, !increasing);
+
+                if (isSafe) {
+                    return true;
+                }
+            }
+        }
+    }
+
+    return false;
+}
 
 int main() {
-    ifstream inputFile("day-2/test.txt");
+    ifstream inputFile("day-2/input.txt");
     vector<vector<int>> data;
     string line;
 
@@ -48,83 +84,33 @@ int main() {
         cerr << "Unable to open file" << endl;
     }
 
+    // Part 1
     int safeCount = 0;
     for (size_t reportIndex = 0; reportIndex < data.size(); ++reportIndex) {
         auto& report = data[reportIndex];
+        bool isSafe = false;
+        bool increasing = true;
 
-        int modifier = find_modifier(report);
-        int magicDiff = 3;
-        cout << "Modifier: " << modifier << endl;
-        cout << "Report size: " << report.size() << endl;
-
-        bool isSafe = true;
-
-        if (abs(modifier) + magicDiff < report.size()) {
-            isSafe = false;
-        } else {
-            bool increasing = modifier > 0;
-
-            bool problemDampner = false;
-            for (int i = 0; i < report.size() - 1; i++) {
-                if (i == report.size() - 1 && !problemDampner){
-                    break;
-                }
-
-                if (increasing == (report[i] - report[i + 1] > 0) || !is_within_range(abs(report[i] - report[i + 1]), 1, 3) ) {
-                   if (!problemDampner) {
-                        problemDampner = true;
-
-                        if (report[i] > report[i + 2]) {
-                           report.erase(report.begin() + i);
-
-                        }
-                        else {
-                            report.erase(report.begin() + i + 1);
-                            i--;
-                        }
-                    } else {
-                        isSafe = false;
-                    }
-                } else if (!increasing == (report[i] - report[i + 1] < 0) || !is_within_range(abs(report[i] - report[i + 1]), 1, 3)) {
-                    if (!problemDampner) {
-                        problemDampner = true;
-
-                        if (report[i] < report[i + 2]) {
-                           report.erase(report.begin() + i);
-
-                        }
-                        else {
-                            report.erase(report.begin() + i + 1);
-                            i--;
-                        }
-                    } else {
-                        isSafe = false;
-                    }
-                }
-
-                // if (!is_within_range(report[i] - report[i + 1], 1, 3)) {
-                //     if (!problemDampner) {
-                //         problemDampner = true;
-                //         report.erase(report.begin() + i + 1);
-                //         i--;
-                //     } else {
-                //         isSafe = false;
-                //     }
-                // }
-            }
-        }
-
+        isSafe = isSafe || safe_check(report, increasing);
+        isSafe = isSafe || safe_check(report, !increasing);
 
         safeCount += isSafe;
-
-        if (isSafe) {
-            cout << "Report index " << reportIndex << ": Safe\n";
-        } else {
-            cout << "Report index " << reportIndex << ": Unsafe\n";
-        }
     }
 
     cout << "Safe count: " << safeCount << endl;
+
+
+    // Part 2
+    int safeCount2 = 0;
+    for (size_t reportIndex = 0; reportIndex < data.size(); ++reportIndex) {
+        auto& report = data[reportIndex];
+        bool isSafe = true;
+        isSafe = brute_check_report(report);
+
+        safeCount2 += isSafe;
+    }
+
+    cout << "Safe count 2: " << safeCount2 << endl;
 
     return 0;
 }
